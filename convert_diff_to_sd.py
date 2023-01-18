c53fbf5a9766b905db9efd2dd4ddb7d06a2aad37
Evgeny Vlasov <e.vlasov@rj-games.com>
2023-01-18 13:06:08 +0200

    New converter interface

--- convert_diff_to_sd.py
+++ convert_diff_to_sd.py
@@ -262,25 +262,10 @@ def convert_text_enc_state_dict(text_enc_dict):
     return text_enc_dict
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--model_path", default=None, type=str, required=True, help="Path to the model to convert.")
-    parser.add_argument("--checkpoint_path", default=None, type=str, required=True, help="Path to the output model.")
-    parser.add_argument("--half", action="store_true", help="Save weights in half precision.")
-    parser.add_argument(
-        "--use_safetensors", action="store_true", help="Save weights use safetensors, default is ckpt."
-    )
-
-    args = parser.parse_args()
-
-    assert args.model_path is not None, "Must provide a model path!"
-
-    assert args.checkpoint_path is not None, "Must provide a checkpoint path!"
-
-    unet_path = osp.join(args.model_path, "unet", "diffusion_pytorch_model.bin")
-    vae_path = osp.join(args.model_path, "vae", "diffusion_pytorch_model.bin")
-    text_enc_path = osp.join(args.model_path, "text_encoder", "pytorch_model.bin")
+def convert(model_path, checkpoint_path, half, use_safetensors):
+    unet_path = osp.join(model_path, "unet", "diffusion_pytorch_model.bin")
+    vae_path = osp.join(model_path, "vae", "diffusion_pytorch_model.bin")
+    text_enc_path = osp.join(model_path, "text_encoder", "pytorch_model.bin")
 
     # Convert the UNet model
     unet_state_dict = torch.load(unet_path, map_location="cpu")
@@ -309,11 +294,11 @@ if __name__ == "__main__":
 
     # Put together new checkpoint
     state_dict = {**unet_state_dict, **vae_state_dict, **text_enc_dict}
-    if args.half:
+    if half:
         state_dict = {k: v.half() for k, v in state_dict.items()}
 
-    if args.use_safetensors:
-        save_file(state_dict, args.checkpoint_path)
+    if use_safetensors:
+        save_file(state_dict, checkpoint_path)
     else:
         state_dict = {"state_dict": state_dict}
-        torch.save(state_dict, args.checkpoint_path)
+        torch.save(state_dict, checkpoint_path)
