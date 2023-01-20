7ddddf3bb48db2d371383b101f42507725fc555c
Evgeny Vlasov <e.vlasov@rj-games.com>
2023-01-20 15:20:00 +0200

    fix load models

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python3 load_models.py --models="models.yaml" --output="models"
+python3 scripts/load_models.py --models="models.yaml" --output="models"
 docker build -t ${1} .
 docker push ${1}
